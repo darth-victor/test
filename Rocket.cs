97fa962817622feaac616bc360b985b35a02ddee
Victor McDonald <VictorM@op.ac.nz>
2024-02-14 14:37:19 +1300

    I done made a file.

---
+++ Rocket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Rocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.WriteLine("\t");
+                }
+            }
+            Console.WriteLine("...");
+            Console.ReadLine();
+        }
+    }
+}
